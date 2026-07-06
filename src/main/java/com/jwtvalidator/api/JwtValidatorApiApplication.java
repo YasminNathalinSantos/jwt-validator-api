6f5734fc47d985e4cf89001075eeaf1d502430ba
YasminNathalinSantos <yasminnathalinsantos@gmail.com>
2026-07-06 14:05:03 -0300

    Preparando ambinete para desenvolvimento

---
+++ src/main/java/com/jwtvalidator/api/JwtValidatorApiApplication.java
@@ -0,0 +1,13 @@
+package com.jwtvalidator.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtValidatorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtValidatorApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jwtvalidator/api/JwtValidatorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jwtvalidator.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtValidatorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
